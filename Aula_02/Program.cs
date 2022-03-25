70933459ef7fe3993911711184c0df4e35ee3e89
RamiroNoronha <100360738+RamiroNoronha@users.noreply.github.com>
2022-03-24 22:57:28 -0300

    Salvando informações do Program.cs

--- Aula_02/Program.cs
+++ Aula_02/Program.cs
@@ -8,12 +8,6 @@ namespace Aula_02
         public static void Main(string[] args)
         {
             string url = "https:www.bytebank.com/cambio?origem=real&destino=dolar";
-            //int indiceInterrogacao = url.IndexOf('?');
-
-            //string argumentos = url.Substring(indiceInterrogacao + 1);
-
-            //Console.WriteLine(argumentos);
-            // Console.WriteLine(indiceInterrogacao);
 
             ExtratorValorDeArgumentosURL extrato = new ExtratorValorDeArgumentosURL(url);
 
